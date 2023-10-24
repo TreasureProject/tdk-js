13b4a2f09e42ef38e9f2bfefb5ab9a94917d79b0
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-10-24 12:49:22 -0700

    move translations to separate file

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import "./globals.css";
+import { en } from "./locales";
 
 export * from "@treasure/core";
 export * from "./context";
@@ -10,37 +11,7 @@ export * from "./hooks";
 export * from "./icons";
 
 i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        common: {
-          loading: "Loading...",
-          close: "Close",
-          items_one: "1 item",
-          items_other: "{{count}} items",
-          removeItem: "Remove item",
-          total: "Total",
-          moreInfo: "More info",
-          insufficientBalance: "Insufficient balance",
-        },
-        payments: {
-          cart: {
-            title: "Checkout Overview",
-            title_success: "Transaction Completed",
-            terms:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-            termsCopy: "© 2021-2023 Treasure. All Rights Reserved.",
-            optionsTitle: "Select payment token:",
-            submit: "Check out",
-            approveAndSubmit: "Approve & check out",
-            successMessageTitle: "Thanks for your order!",
-            successMessageDescription:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-          },
-        },
-      },
-    },
-  },
+  resources: { en },
   lng: "en",
   fallbackLng: "en",
   interpolation: {
---
+++ packages/react/src/locales/en.ts
@@ -0,0 +1,29 @@
+export const en = {
+  translation: {
+    common: {
+      loading: "Loading...",
+      close: "Close",
+      items_one: "1 item",
+      items_other: "{{count}} items",
+      removeItem: "Remove item",
+      total: "Total",
+      moreInfo: "More info",
+      insufficientBalance: "Insufficient balance",
+    },
+    payments: {
+      cart: {
+        title: "Checkout Overview",
+        title_success: "Transaction Completed",
+        terms:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        termsCopy: "© 2021-2023 Treasure. All Rights Reserved.",
+        optionsTitle: "Select payment token:",
+        submit: "Check out",
+        approveAndSubmit: "Approve & check out",
+        successMessageTitle: "Thanks for your order!",
+        successMessageDescription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+      },
+    },
+  },
+};
