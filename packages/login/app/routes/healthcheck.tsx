985b52578257f1744053c68a670340371559fb1f
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-02-01 20:53:17 -0800

    update login healthcheck route

--- packages/login/app/routes/healthcheck.tsx
+++ packages/login/app/routes/healthcheck.tsx
@@ -1,17 +1,1 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
-
-  try {
-    const url = new URL("/", `http://${host}`);
-    await fetch(url.toString(), { method: "HEAD" }).then((r) => {
-      if (!r.ok) return Promise.reject(r);
-    });
-    return new Response("OK");
-  } catch (error: unknown) {
-    console.log("healthcheck ❌", { error });
-    return new Response("ERROR", { status: 500 });
-  }
-};
+export const loader = () => new Response("OK");
