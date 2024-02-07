cef746abd719fc41d28139202f8343e90027f138
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-02-07 15:36:50 -0800

    upgrade viem and wagmi on harvester example

--- examples/harvester/src/main.tsx
+++ examples/harvester/src/main.tsx
@@ -1,50 +1,43 @@
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@treasure-project/tailwind-config/fonts.css";
 import { TreasureProvider } from "@treasure/tdk-react";
 import "@treasure/tdk-react/dist/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { WagmiProvider } from "wagmi";
 import { arbitrumSepolia } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
 
 import { App } from "./App.tsx";
 import "./index.css";
 
-const { chains, publicClient } = configureChains(
-  [arbitrumSepolia],
-  [publicProvider()],
-);
-
-const { connectors } = getDefaultWallets({
+const config = getDefaultConfig({
   appName: "TDK Harvester Example",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains,
+  chains: [arbitrumSepolia],
 });
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <TreasureProvider
-          project="platform"
-          chainId={import.meta.env.VITE_TDK_CHAIN_ID}
-          apiUri={import.meta.env.VITE_TDK_API_URL}
-          authConfig={{
-            loginDomain: import.meta.env.VITE_TDK_LOGIN_DOMAIN,
-            redirectUri: import.meta.env.VITE_TDK_LOGIN_REDIRECT_URI,
-          }}
-        >
-          <App />
-        </TreasureProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <TreasureProvider
+            project="platform"
+            chainId={import.meta.env.VITE_TDK_CHAIN_ID}
+            apiUri={import.meta.env.VITE_TDK_API_URL}
+            authConfig={{
+              loginDomain: import.meta.env.VITE_TDK_LOGIN_DOMAIN,
+              redirectUri: import.meta.env.VITE_TDK_LOGIN_REDIRECT_URI,
+            }}
+          >
+            <App />
+          </TreasureProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 );
