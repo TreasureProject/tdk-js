08a6514c35c0cbe0abdf84b0074711e308ec03f3
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-02-10 18:00:44 -0800

    upgrade payments example to wagmi v2 (#7)

--- examples/payments/src/main.tsx
+++ examples/payments/src/main.tsx
@@ -1,50 +1,40 @@
 import {
   RainbowKitProvider,
-  connectorsForWallets,
   darkTheme,
+  getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@treasure-project/tailwind-config/fonts.css";
 import "@treasure/tdk-react/dist/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { WagmiProvider } from "wagmi";
 import { arbitrumSepolia } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
 
 import { App } from "./App.tsx";
 import "./index.css";
 
-const { chains, publicClient } = configureChains(
-  [arbitrumSepolia],
-  [publicProvider()],
-);
-
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [injectedWallet({ chains })],
-  },
-]);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
+const config = getDefaultConfig({
+  appName: "TDK Payments Example",
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+  chains: [arbitrumSepolia],
 });
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={darkTheme({
-          accentColor: "#DC2626",
-        })}
-      >
-        <App />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#DC2626",
+          })}
+        >
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 );
