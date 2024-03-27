ec351f207bc387537dcb53de10bd5a84e9d5957e
mulf <mhmulford0@gmail.com>
2024-03-27 17:45:07 -0500

    tabs and stories

---
+++ packages/design-system/src/components/tabs/AriaTabs.stories.tsx
@@ -0,0 +1,10 @@
+// Replace your-framework with the name of your framework
+import AriaTabs from './AriaTabs';
+const meta = {
+  component: AriaTabs,
+};
+
+export const AriaTabsStory = {
+  args: {},
+};
+export default meta;
---
+++ packages/design-system/src/components/tabs/RadixTabs.stories.tsx
@@ -0,0 +1,10 @@
+// Replace your-framework with the name of your framework
+import RadixTabs from './RadixTabs';
+const meta = {
+  component: RadixTabs,
+};
+
+export const RadixTabsStory = {
+  args: {},
+};
+export default meta;
