183a48b217f276ef78d40c945a646890dd3fa7ee
mulf <mhmulford0@gmail.com>
2024-03-27 14:24:48 -0500

    add new btn

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -1,7 +1,9 @@
 export function Button() {
   return (
-    <div className='bg-red-500 w-32'>
-      <p>hello world from the button</p>
-    </div>
+    <button
+      type='button'
+      className='rounded bg-indigo-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+      Button text
+    </button>
   );
 }
