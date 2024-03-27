b74717d9b84b5b90b41c84a97c3c1b005a4a1ead
mulf <mhmulford0@gmail.com>
2024-03-27 17:31:34 -0500

    modals and popovers with stories

---
+++ packages/design-system/src/components/popover/AriaPop.stories.tsx
@@ -0,0 +1,12 @@
+// Replace your-framework with the name of your framework
+
+import AriaProp from './AriaPop';
+
+const meta = {
+  component: AriaProp,
+};
+
+export const AriaPropStory = {
+  args: {},
+};
+export default meta;
---
+++ packages/design-system/src/components/popover/RadixPop.stories.tsx
@@ -0,0 +1,11 @@
+// Replace your-framework with the name of your framework
+
+import RadixPop from './RadixPop';
+const meta = {
+  component: RadixPop,
+};
+
+export const RadixPopStory = {
+  args: {},
+};
+export default meta;
