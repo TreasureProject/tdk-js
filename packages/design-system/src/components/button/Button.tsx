471e71e8c4c499a938e3d95becf3d41debc80ab9
mulf <mhmulford0@gmail.com>
2024-03-27 14:53:46 -0500

    fmt

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -1,9 +1,9 @@
 export function Button() {
-  return (
-    <button
-      type='button'
-      className='rounded bg-indigo-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-      Button text
-    </button>
-  );
+    return (
+        <button
+            type='button'
+            className='rounded bg-indigo-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+            Button text
+        </button>
+    );
 }
