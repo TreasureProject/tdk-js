d1836d08182c1fbeaea0f7bd5284bcaa137a6878
mulf <mhmulford0@gmail.com>
2024-04-24 17:15:30 -0500

    btn foundation

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -1,20 +1,57 @@
+/* eslint-disable react/prop-types */
+
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+type ButtonType = 'link' | 'compact' | 'base';
+type ButtonSize = 'xs' | 'sm' | 'base' | 'lg';
+type ButtonColor = 'ruby' | 'honey' | 'night' | 'silver' | 'nebula';
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: 'small' | 'medium' | 'large';
-  color?: 'primary' | 'secondary' | 'tertiary';
+  btnType: ButtonType;
+  size: ButtonSize;
+  color: ButtonColor;
   className?: string;
-  children: React.ReactNode;
+  leftIcon?: boolean;
+  rightIcon?: boolean;
+  children?: React.ReactNode;
 }
 
-export function Button() {
+const buttonTypeClasses: Record<ButtonType, string> = {
+  link: 'link styles heres',
+  compact: 'compact styles here',
+  base: 'base btn style',
+};
+
+const buttonSizeClasses: Record<ButtonSize, string> = {
+  xs: '',
+  sm: '',
+  base: '',
+  lg: '',
+};
+
+const buttonColorClasses: Record<ButtonColor, string> = {
+  ruby: '',
+  honey: '',
+  night: '',
+  silver: '',
+  nebula: '',
+};
+
+const Button: React.FC<ButtonProps> = ({ className, children, size, color, btnType, ...props }) => {
+  const buttonClasses = twMerge(
+    'rounded font-semibold',
+    buttonSizeClasses[size],
+    buttonColorClasses[color],
+    buttonTypeClasses[btnType],
+    className
+  );
+
   return (
-    <button
-      type='button'
-      className=' rounded bg-ruby-500 px-4 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-    >
-      Button text
+    <button className={clsx(buttonClasses)} {...props}>
+      {children}
     </button>
   );
-}
+};
+
+export default Button;
