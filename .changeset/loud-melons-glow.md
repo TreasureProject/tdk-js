7494c70bbb83d6b5c9a596317a98a639b7c6f0b5
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-07-28 22:00:53 -0700

    react: add farcaster social auth option

---
+++ .changeset/loud-melons-glow.md
@@ -0,0 +1,5 @@
+---
+"@treasure-dev/tdk-react": patch
+---
+
+Added Farcaster social auth option
--- packages/react/src/utils/wallet.ts
+++ packages/react/src/utils/wallet.ts
@@ -3,7 +3,15 @@ import { inAppWallet } from "thirdweb/wallets";
 export const SUPPORTED_WALLETS = [
   inAppWallet({
     auth: {
-      options: ["email", "google", "apple", "discord", "passkey", "phone"],
+      options: [
+        "email",
+        "google",
+        "apple",
+        "discord",
+        "farcaster",
+        "passkey",
+        "phone",
+      ],
     },
   }),
 ];
