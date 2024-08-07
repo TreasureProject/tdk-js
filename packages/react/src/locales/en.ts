5be2bd37b8d1a2a871f923272266ac78ba0381d2
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-08-07 13:48:01 -0700

    remove unused translations

--- packages/react/src/locales/en.ts
+++ packages/react/src/locales/en.ts
@@ -1,19 +1,10 @@
 export const en = {
   translation: {
     common: {
-      loading: "Loading...",
       close: "Close",
-      items_one: "1 item",
-      items_other: "{{count}} items",
-      removeItem: "Remove item",
-      total: "Total",
-      moreInfo: "More info",
-      insufficientBalance: "Insufficient balance",
-      copyAddress: "Copy Address",
     },
     login: {
       connect: "Connect",
-      disconnect: "Disconnect",
     },
   },
 };
