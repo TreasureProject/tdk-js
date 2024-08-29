550a3198ed80c079c124ef0490e0aa9f698dd5a6
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-08-29 16:39:29 -0700

    react: streamline error messages in connect flow (#108)

--- packages/react/src/utils/error.ts
+++ packages/react/src/utils/error.ts
@@ -1,2 +1,15 @@
-export const getErrorMessage = (err: unknown) =>
-  err instanceof Error ? err.message : String(err);
+export const getErrorMessage = (err: unknown) => {
+  const message = err instanceof Error ? err.message : String(err);
+
+  // Skip displaying certain error messages
+  if (
+    // Sent from Thirdweb if the user closes the social login window
+    message === "User closed login window" ||
+    // Sent from passkey login if the user cancels the process
+    (err instanceof Error && err.name === "NotAllowedError")
+  ) {
+    return "";
+  }
+
+  return message;
+};
