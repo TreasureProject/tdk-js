4af89a2463291fe270ca15c0f2aee00bd88ee4f6
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-09-26 14:26:39 -0700

    api: update sentry tracking and upgrade packages

--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -1,15 +1,10 @@
 import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 const environment = process.env.SENTRY_ENVIRONMENT || "production";
-const isDevelopment = environment === "development";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment,
-  integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
-  sampleRate: isDevelopment ? 1 : 0.8,
-  // instrumentation fails when included: https://github.com/getsentry/sentry-javascript/issues/13662
-  // tracesSampleRate: isDevelopment ? 1 : 0.6,
-  profilesSampleRate: isDevelopment ? 1 : 0.6,
+  sampleRate: environment === "development" ? 1 : 0.75,
+  ignoreErrors: ["Unauthorized", "401 Unauthorized", "404 Not Found"],
 });
