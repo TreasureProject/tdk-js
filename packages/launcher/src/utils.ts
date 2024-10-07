b6e0587735e5f7eda1a22a94a74a04d3b1e4d885
Wyatt Mufson <wyatt@ryu.games>
2024-10-07 17:44:05 -0400

    improve search function

--- packages/launcher/src/utils.ts
+++ packages/launcher/src/utils.ts
@@ -1,11 +1,20 @@
 export function getTreasureLauncherAuthToken(): string | undefined {
-  const windowAuthToken = window.location.search.match(
-    /--tdk-auth-token=(\w+)/,
-  )?.[1];
-  const processBackendWallet = process.argv
-    .find((arg) => arg.startsWith("--tdk-auth-token="))
-    ?.split("=")[1];
-  return windowAuthToken || processBackendWallet;
+  let args: string[] | undefined;
+
+  if (typeof process !== "undefined" && Array.isArray(process.argv)) {
+    args = process.argv;
+  } else if (
+    typeof window !== "undefined" &&
+    window.process &&
+    Array.isArray(window.process.argv)
+  ) {
+    args = window.process.argv;
+  } else {
+    return undefined;
+  }
+
+  const arg = args.find((arg) => arg.startsWith("--tdk-auth-token="));
+  return arg ? arg.split("=")[1] : undefined;
 }
 
 export function isUsingTreasureLauncher(): boolean {
