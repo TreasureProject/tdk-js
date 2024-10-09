1a94272594a6de1772cb4f8aaa04781dcfff86b5
Wyatt Mufson <wyatt@ryu.games>
2024-10-09 18:56:15 -0400

    implement id generation

--- packages/core/src/analytics/utils.ts
+++ packages/core/src/analytics/utils.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 export async function getServerTime(apiUri: string): Promise<string> {
   const result = await fetch(`${apiUri}/utils/time-unix`);
   if (!result.ok) {
@@ -8,5 +10,5 @@ export async function getServerTime(apiUri: string): Promise<string> {
 }
 
 export function getEventId(): string {
-  return "123";
+  return `event-${uuidv4()}`;
 }
