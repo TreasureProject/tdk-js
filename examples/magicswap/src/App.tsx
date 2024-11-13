c08b21a7a7c3418c9f554d5642d7ad5a91ac9364
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-11-12 18:45:56 -0800

    split pool list and details into pages

--- examples/magicswap/src/App.tsx
+++ examples/magicswap/src/App.tsx
@@ -5,7 +5,8 @@ import {
 } from "@treasure-dev/tdk-core";
 import { Button, ConnectButton, useTreasure } from "@treasure-dev/tdk-react";
 import { useEffect, useState } from "react";
-import { toTokens, toUnits } from "thirdweb";
+import { getContract, toTokens, toUnits, ZERO_ADDRESS } from "thirdweb";
+import { allowance, balanceOf } from "thirdweb/extensions/erc20";
 
 const useMagicswapPools = () => {
   const { tdk } = useTreasure();
@@ -100,54 +101,87 @@ const useMagicswapRoute = ({
   return state;
 };
 
-const WMAGIC_USDC_POOL_ADDRESS = "0x9c61210b8c8ea450bd3fdbd7a7c1208206d18b7b";
-const USDC_ADDRESS = "0x99b9ed17bb37768bb1a3cb6d91b15834eb7c2185";
-const WMAGIC_ADDRESS = "0x095ded714d42cbd5fb2e84a0ffbfb140e38dc9e1";
-
-export const App = () => {
+export const PoolDetailsPage = ({
+  id,
+  addLog,
+  onBack,
+}: { id: string; addLog: (log: string) => void; onBack: () => void }) => {
   const [amount, setAmount] = useState("10");
-  const [tokenInId, setTokenInId] = useState(USDC_ADDRESS);
-  const [logs, setLogs] = useState<string[]>([]);
-  const tokenOutId =
-    tokenInId === WMAGIC_ADDRESS ? USDC_ADDRESS : WMAGIC_ADDRESS;
+  const [tokenInId, setTokenInId] = useState<string | undefined>();
 
-  const { tdk, contractAddresses, userAddress } = useTreasure();
-  const { pools, isLoading: isLoadingPools } = useMagicswapPools();
-  const { pool, isLoading: isLoadingPool } = useMagicswapPool(
-    WMAGIC_USDC_POOL_ADDRESS,
-  );
+  const { client, chain, tdk, contractAddresses, userAddress } = useTreasure();
+  const { pool, isLoading: isLoadingPool } = useMagicswapPool(id);
+
+  const tokenOutId =
+    pool?.token0.id === tokenInId ? pool?.token1.id : pool?.token0.id;
+
   const { route } = useMagicswapRoute({
-    tokenInId,
-    tokenOutId,
+    tokenInId: tokenInId ?? ZERO_ADDRESS,
+    tokenOutId: tokenOutId ?? ZERO_ADDRESS,
     amount,
     isExactOut: false,
   });
 
-  const addLog = (log: string) => {
-    setLogs((curr) => [log, ...curr]);
-  };
+  useEffect(() => {
+    if (pool?.token0.id) {
+      setTokenInId(pool.token0.id);
+    }
+  }, [pool?.token0.id]);
 
   const handleSwap = async () => {
-    if (!route) {
-      return;
+    if (!userAddress) {
+      throw new Error("No user connected");
     }
 
-    addLog(`Approving ${amount} ${route.tokenIn.symbol} for trading...`);
-    try {
-      const result = await tdk.transaction.create({
-        address: tokenInId,
-        abi: erc20Abi,
-        functionName: "approve",
-        args: [
-          contractAddresses.MagicswapV2Router,
-          toUnits(amount, route.tokenIn.decimals),
-        ],
-      });
-      addLog(
-        `Successfully approved ${route.tokenIn.symbol}: ${"transactionHash" in result ? result.transactionHash : "Unknown transaction"}`,
-      );
-    } catch (err) {
-      addLog(`Error approving ${route.tokenIn.symbol}: ${err}`);
+    if (!tokenInId || !route) {
+      throw new Error("No swap route configured");
+    }
+
+    const amountBI = BigInt(toUnits(amount, route.tokenIn.decimals));
+
+    const tokenInContract = getContract({
+      client,
+      chain,
+      address: tokenInId,
+      abi: erc20Abi,
+    });
+
+    const [tokenInBalance, tokenInAllowance] = await Promise.all([
+      balanceOf({
+        contract: tokenInContract,
+        address: userAddress,
+      }),
+      allowance({
+        contract: tokenInContract,
+        owner: userAddress,
+        spender: contractAddresses.MagicswapV2Router,
+      }),
+    ]);
+
+    if (tokenInBalance < amountBI) {
+      throw new Error(`Insufficient ${route.tokenIn.symbol} balance`);
+    }
+
+    if (tokenInAllowance < amountBI) {
+      addLog(`Approving ${amount} ${route.tokenIn.symbol} for trading...`);
+      try {
+        const result = await tdk.transaction.create({
+          address: tokenInId,
+          abi: erc20Abi,
+          functionName: "approve",
+          args: [
+            contractAddresses.MagicswapV2Router,
+            toUnits(amount, route.tokenIn.decimals),
+          ],
+        });
+        addLog(
+          `Successfully approved ${route.tokenIn.symbol}: ${"transactionHash" in result ? result.transactionHash : "Unknown transaction"}`,
+        );
+      } catch (err) {
+        throw new Error(`Error approving ${route.tokenIn.symbol}: ${err}`);
+      }
+    } else {
+      addLog(`${route.tokenIn.symbol} is already approved for trading`);
     }
 
     addLog(
@@ -167,10 +201,136 @@ export const App = () => {
         `Successfully swapped ${route.tokenIn.symbol} for ${route.tokenOut.symbol}: ${"transactionHash" in result ? result.transactionHash : "Unknown transaction"}`,
       );
     } catch (err) {
-      addLog(
+      throw new Error(
         `Error swapping ${route.tokenIn.symbol} for ${route.tokenOut.symbol}: ${err}`,
       );
     }
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="underline hover:no-underline"
+        onClick={() => onBack()}
+      >
+        &laquo; Back
+      </button>
+      <h2 className="font-medium text-2xl">Pool Details</h2>
+      {pool ? (
+        <>
+          <p>
+            {pool.name} ({pool.id})
+          </p>
+          <div>
+            <h3 className="text-lg">Tokens</h3>
+            <ul className="list-disc pl-6">
+              <li>
+                {pool.token0.symbol} ({pool.token0.id})
+              </li>
+              <li>
+                {pool.token1.symbol} ({pool.token1.id})
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-lg">Reserves</h3>
+            <ul className="list-disc pl-6">
+              <li>
+                {pool.reserve0} {pool.token0.symbol}
+              </li>
+              <li>
+                {pool.reserve1} {pool.token1.symbol}
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-1">
+            <h2 className="font-medium text-2xl">Swap</h2>
+            <div className="flex items-center">
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) =>
+                  setAmount(
+                    Number.isNaN(e.target.value) ? "0" : e.target.value || "0",
+                  )
+                }
+              />
+              <select
+                value={tokenInId}
+                onChange={(e) => setTokenInId(e.target.value)}
+              >
+                <option value={pool.token0.id}>{pool.token0.symbol}</option>
+                <option value={pool.token1.id}>{pool.token1.symbol}</option>
+              </select>{" "}
+              ={" "}
+              {route ? (
+                <>
+                  {toTokens(BigInt(route.amountOut), route.tokenOut.decimals)}{" "}
+                  {route.tokenOut.symbol}
+                </>
+              ) : (
+                "Calculating..."
+              )}
+            </div>
+            <Button
+              onClick={async () => {
+                try {
+                  await handleSwap();
+                } catch (err) {
+                  addLog(
+                    err instanceof Error
+                      ? err.message
+                      : "Unknown error occurred",
+                  );
+                }
+              }}
+            >
+              Swap
+            </Button>
+          </div>
+        </>
+      ) : (
+        <p>{isLoadingPool ? "Loading..." : "Pool not found"}</p>
+      )}
+    </div>
+  );
+};
+
+export const PoolListPage = ({
+  onSelect,
+}: { onSelect: (id: string) => void }) => {
+  const { pools, isLoading: isLoadingPools } = useMagicswapPools();
+  return (
+    <div>
+      <h2 className="font-medium text-2xl">Pools</h2>
+      {pools.length > 0 ? (
+        <ul className="list-disc pl-6">
+          {pools.map((pool) => (
+            <li key={pool.id}>
+              <button
+                type="button"
+                className="underline hover:no-underline"
+                onClick={() => onSelect(pool.id)}
+              >
+                {pool.name} ({pool.id})
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>{isLoadingPools ? "Loading..." : "None"}</p>
+      )}
+    </div>
+  );
+};
+
+export const App = () => {
+  const [selectedPoolId, setSelectedPoolId] = useState<string | undefined>();
+  const [logs, setLogs] = useState<string[]>([]);
+
+  const addLog = (log: string) => {
+    setLogs((curr) => [log, ...curr]);
   };
 
   return (
@@ -182,85 +342,15 @@ export const App = () => {
         <ConnectButton />
       </header>
       <main className="space-y-6">
-        <div>
-          <h2 className="font-medium text-2xl">Pools</h2>
-          {pools.length > 0 ? (
-            <ul className="list-disc pl-6">
-              {pools.map((pool) => (
-                <li key={pool.id}>
-                  {pool.name} ({pool.id})
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>{isLoadingPools ? "Loading..." : "None"}</p>
-          )}
-        </div>
-        <div>
-          <h2 className="font-medium text-2xl">Pool Details</h2>
-          {pool ? (
-            <>
-              <p>
-                {pool.name} ({pool.id})
-              </p>
-              <div>
-                <h3 className="text-lg">Tokens</h3>
-                <ul className="list-disc pl-6">
-                  <li>
-                    {pool.token0.symbol} ({pool.token0.id})
-                  </li>
-                  <li>
-                    {pool.token1.symbol} ({pool.token1.id})
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-lg">Reserves</h3>
-                <ul className="list-disc pl-6">
-                  <li>
-                    {pool.reserve0} {pool.token0.symbol}
-                  </li>
-                  <li>
-                    {pool.reserve1} {pool.token1.symbol}
-                  </li>
-                </ul>
-              </div>
-            </>
-          ) : (
-            <p>{isLoadingPool ? "Loading..." : "Pool not found"}</p>
-          )}
-        </div>
-        <div className="space-y-1">
-          <h2 className="font-medium text-2xl">Swap</h2>
-          <div className="flex items-center">
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) =>
-                setAmount(
-                  Number.isNaN(e.target.value) ? "0" : e.target.value || "0",
-                )
-              }
-            />
-            <select
-              value={tokenInId}
-              onChange={(e) => setTokenInId(e.target.value)}
-            >
-              <option value={USDC_ADDRESS}>USDC</option>
-              <option value={WMAGIC_ADDRESS}>WMAGIC</option>
-            </select>{" "}
-            ={" "}
-            {route ? (
-              <>
-                {toTokens(BigInt(route.amountOut), route.tokenOut.decimals)}{" "}
-                {route.tokenOut.symbol}
-              </>
-            ) : (
-              "Calculating..."
-            )}
-          </div>
-          <Button onClick={handleSwap}>Swap</Button>
-        </div>
+        {selectedPoolId ? (
+          <PoolDetailsPage
+            id={selectedPoolId}
+            addLog={addLog}
+            onBack={() => setSelectedPoolId(undefined)}
+          />
+        ) : (
+          <PoolListPage onSelect={setSelectedPoolId} />
+        )}
         {logs.length > 0 ? (
           <div className="space-y-1">
             <h1 className="font-medium text-xl">Logs</h1>
