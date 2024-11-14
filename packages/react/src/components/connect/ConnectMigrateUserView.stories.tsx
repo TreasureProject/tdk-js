183b53bc34e8737756be5de363603b1d5994a552
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-11-14 13:58:26 -0800

    update copy

--- packages/react/src/components/connect/ConnectMigrateUserView.stories.tsx
+++ packages/react/src/components/connect/ConnectMigrateUserView.stories.tsx
@@ -19,6 +19,7 @@ export const Default: Story = {
         pfp: "https://djmahssgw62sw.cloudfront.net/general/0xb8e0d594cd869e49ae55c5b44fa886857b1cdeb9d4aeb49b44d47eeccf97c835.png",
         banner:
           "https://djmahssgw62sw.cloudfront.net/general/0x19a59e64df51da1aad93ee9d0c4ec00412c0de13b2401be65ee775714f786dd4.jpg",
+        legacyAddress: "0x0DF42DB01fF1992FBd2aCff3b7a9010CF59B6F80",
       },
       {
         id: "test-legacyProfile-2",
@@ -26,6 +27,15 @@ export const Default: Story = {
         discriminant: 0,
         pfp: null,
         banner: null,
+        legacyAddress: "0x0DF42DB01fF1992FBd2aCff3b7a9010CF59B6F80",
+      },
+      {
+        id: "test-legacyProfile-3",
+        tag: null,
+        discriminant: null,
+        pfp: null,
+        banner: null,
+        legacyAddress: "0x0DF42DB01fF1992FBd2aCff3b7a9010CF59B6F80",
       },
     ],
   },
--- packages/react/src/components/connect/ConnectMigrateUserView.tsx
+++ packages/react/src/components/connect/ConnectMigrateUserView.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { shortenAddress } from "thirdweb/utils";
 
-import { TreasureIcon } from "../../icons/TreasureIcon";
+import { TreasureSparklesIcon } from "../../icons/TreasureSparklesIcon";
 import { cn } from "../../utils/classnames";
 import { Button } from "../ui/Button";
 
@@ -11,6 +12,7 @@ type Props = {
     discriminant: number | null;
     pfp: string | null;
     banner: string | null;
+    legacyAddress: string;
   }[];
   error?: string;
   onApprove: (id: string) => void;
@@ -23,7 +25,6 @@ export const ConnectMigrateUserView = ({
   error,
   onApprove,
   onReject,
-  onCancel,
 }: Props) => {
   const [selectedProfileId, setSelectedProfileId] = useState(
     legacyProfiles[0]?.id,
@@ -36,96 +37,91 @@ export const ConnectMigrateUserView = ({
           className="tdk-text-lg tdk-font-semibold tdk-text-white tdk-m-0"
           aria-hidden="true"
         >
-          Migrate your account
+          Migrate existing accounts
         </h2>
         <p className="tdk-text-sm tdk-text-silver">
-          {legacyProfiles.length > 1 ? (
-            <>
-              We found the following matching profiles from the Treasure Market.
-              Please select one you would like to merge with your Treasure
-              Account and use as your identity across the Treasure ecosystem:
-            </>
-          ) : (
-            <>
-              We found the following matching profile from the Treasure Market.
-              Would you like to merge it with your Treasure Account and use it
-              as your identity across the Treasure ecosystem?
-            </>
-          )}
+          It looks like you have several existing Treasure profiles! Please
+          choose one you would like to use moving forward as your identity
+          across the Treasure ecosystem.
         </p>
       </div>
       <div className="tdk-flex tdk-items-center tdk-gap-3 tdk-flex-wrap">
-        {legacyProfiles.map(({ id, tag, discriminant, pfp, banner }) => (
-          <button
-            key={id}
-            type="button"
-            className={cn(
-              "tdk-relative tdk-px-4 tdk-py-2.5 tdk-rounded-lg tdk-overflow-hidden tdk-bg-night-500 tdk-border-2 tdk-border-solid tdk-border-night-200 tdk-text-left",
-              legacyProfiles.length > 1 &&
-                "tdk-cursor-pointer hover:tdk-border-night-100",
-              legacyProfiles.length > 1 &&
+        {legacyProfiles.map(
+          ({ id, tag, discriminant, pfp, banner, legacyAddress }) => (
+            <button
+              key={id}
+              type="button"
+              className={cn(
+                "tdk-relative tdk-px-4 tdk-py-2.5 tdk-rounded-lg tdk-overflow-hidden tdk-cursor-pointer tdk-bg-night-500 tdk-border-2 tdk-border-solid tdk-border-night-200 hover:tdk-border-night-100 tdk-text-left",
                 id === selectedProfileId &&
-                "tdk-border-ruby-500/60 hover:tdk-border-ruby-500/60 tdk-shadow",
-            )}
-            onClick={() => setSelectedProfileId(id)}
-          >
-            {banner ? (
-              <div className="tdk-absolute tdk-inset-0">
+                  "tdk-border-ruby-500/60 hover:tdk-border-ruby-500/60 tdk-shadow",
+              )}
+              onClick={() => setSelectedProfileId(id)}
+            >
+              {banner ? (
+                <div className="tdk-absolute tdk-inset-0">
+                  <img
+                    src={banner}
+                    alt=""
+                    className="tdk-w-full tdk-h-full tdk-object-cover tdk-rounded-t-lg"
+                  />
+                  <div className="tdk-absolute tdk-inset-0 tdk-bg-gradient-to-b tdk-from-transparent tdk-to-night-1000/90" />
+                </div>
+              ) : null}
+              <div className="tdk-relative tdk-space-y-1.5">
                 <img
-                  src={banner}
+                  src={
+                    pfp ??
+                    "https://images.treasure.lol/tdk/login/treasure_icon.png"
+                  }
                   alt=""
-                  className="tdk-w-full tdk-h-full tdk-object-cover tdk-rounded-t-lg"
+                  className={cn(
+                    "tdk-w-12 tdk-h-12 tdk-rounded-md tdk-border tdk-border-solid tdk-border-night-200",
+                    !pfp && "tdk-opacity-60",
+                  )}
                 />
-                <div className="tdk-absolute tdk-inset-0 tdk-bg-gradient-to-b tdk-from-transparent tdk-to-night-1000/90" />
-              </div>
-            ) : null}
-            <div className="tdk-relative">
-              <img
-                src={
-                  pfp ??
-                  "https://images.treasure.lol/tdk/login/treasure_icon.png"
-                }
-                alt=""
-                className={cn(
-                  "tdk-w-12 tdk-h-12 tdk-rounded-md tdk-border tdk-border-solid tdk-border-night-200",
-                  !pfp && "tdk-opacity-60",
+                {tag && discriminant !== null ? (
+                  <div className="tdk-flex tdk-items-center tdk-gap-0.5">
+                    <TreasureSparklesIcon className="tdk-w-4 tdk-h-4 tdk-text-ruby" />
+                    <span className="tdk-font-semibold tdk-text-cream">
+                      {tag}
+                    </span>
+                    <span className="tdk-text-silver-500">
+                      #{discriminant.toString().padStart(4, "0")}
+                    </span>
+                  </div>
+                ) : (
+                  <div className="tdk-text-silver-500 tdk-font-medium">
+                    {shortenAddress(legacyAddress)}
+                  </div>
                 )}
-              />
-              <div className="tdk-flex tdk-items-center tdk-gap-0.5">
-                <TreasureIcon
-                  className="tdk-w-7 tdk-h-7 tdk-text-transparent"
-                  starsFill="#C62222"
-                />
-                <span className="tdk-font-semibold tdk-text-cream">{tag}</span>
-                {""}
-                <span className="tdk-text-silver-500">
-                  #{discriminant?.toString().padStart(4, "0")}
-                </span>
               </div>
-            </div>
-          </button>
-        ))}
+            </button>
+          ),
+        )}
       </div>
       {error ? (
         <p className="tdk-bg-ruby-200 tdk-border tdk-border-ruby-800 tdk-text-ruby-800 tdk-px-3 tdk-py-2 tdk-rounded-md">
           {error}
         </p>
       ) : null}
-      <div className="tdk-flex tdk-items-center tdk-gap-2">
-        <Button
-          disabled={!selectedProfileId}
-          onClick={
-            selectedProfileId ? () => onApprove(selectedProfileId) : undefined
-          }
-        >
-          Migrate
-        </Button>
-        <Button variant="secondary" onClick={onReject}>
-          Skip
-        </Button>
-        <Button variant="tertiary" onClick={onCancel}>
-          Ask me later
-        </Button>
+      <div className="tdk-space-y-2">
+        <div className="tdk-flex tdk-items-center tdk-gap-2">
+          <Button
+            disabled={!selectedProfileId}
+            onClick={
+              selectedProfileId ? () => onApprove(selectedProfileId) : undefined
+            }
+          >
+            Use this account
+          </Button>
+          <Button variant="secondary" onClick={onReject}>
+            Start fresh
+          </Button>
+        </div>
+        <p className="tdk-text-xs tdk-text-silver-600">
+          NOTE: This is irreversible, so please choose carefully.
+        </p>
       </div>
     </div>
   );
