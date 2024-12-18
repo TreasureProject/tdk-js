b5b9265d9df6bd1668b8fdb03845579335be1965
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-12-18 15:30:39 -0800

    react: fix active wallet missing from TDK API client

---
+++ .changeset/tender-guests-kiss.md
@@ -0,0 +1,5 @@
+---
+"@treasure-dev/tdk-react": patch
+---
+
+Fixed active wallet missing from TDK API client
--- examples/connect-react/src/main.tsx
+++ examples/connect-react/src/main.tsx
@@ -1,14 +1,14 @@
 import "@treasure-dev/tdk-react/dist/index.css";
 import "@treasure-dev/tailwind-config/fonts.css";
 
-import { treasureTopaz } from "@treasure-dev/tdk-core";
+import { getContractAddress, treasureTopaz } from "@treasure-dev/tdk-core";
 import { TreasureProvider } from "@treasure-dev/tdk-react";
 import ReactDOM from "react-dom/client";
+import { toWei } from "thirdweb";
 import { arbitrumSepolia } from "thirdweb/chains";
 import { ThirdwebProvider } from "thirdweb/react";
 
 import { App } from "./App.tsx";
-import { SESSION_OPTIONS_BY_CHAIN_ID } from "./constants";
 import "./index.css";
 
 const root = document.getElementById("root");
@@ -27,7 +27,14 @@ if (root) {
         clientId={import.meta.env.VITE_TDK_CLIENT_ID}
         ecosystemId={import.meta.env.VITE_TDK_ECOSYSTEM_ID}
         ecosystemPartnerId={import.meta.env.VITE_TDK_ECOSYSTEM_PARTNER_ID}
-        sessionOptions={SESSION_OPTIONS_BY_CHAIN_ID[defaultChainId]}
+        sessionOptions={{
+          backendWallet: import.meta.env.VITE_TDK_BACKEND_WALLET,
+          approvedTargets: [
+            getContractAddress(arbitrumSepolia.id, "MAGIC"),
+            "0xE647b2c46365741e85268ceD243113d08F7E00B8", // Treasury
+          ],
+          nativeTokenLimitPerTransaction: toWei("1"),
+        }}
         analyticsOptions={{
           apiKey: import.meta.env.VITE_TDK_ANALYTICS_API_KEY,
           appInfo: {
